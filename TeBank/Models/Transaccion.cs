89be3ef7d4f9a79d3addf592bfe3b2725d4661d3
ElVincents <erick_garcia2016@hotmail.com>
2022-06-13 20:29:36 -0400

    Agregamos Transaccion

--- TeBank/Models/Transaccion.cs
+++ TeBank/Models/Transaccion.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
@@ -9,6 +10,38 @@ namespace TeBank.Models
     [Table("Transaccion")]
     public class Transaccion
     {
+        [Key]
+        public int Id { get; set; }
 
+        public string TransaccionUnico { get; set; }
+
+        public decimal Montotransaccion { get; set; }
+
+        public TransStatus TransactionStatus { get; set; }
+
+        public bool IsSuccessful => TransactionStatus.Equals(TransStatus.Exitoso);//Este exitoso funciona gracias a linea 40
+
+        public string Cuentaemisor { get; set; }
+
+        public string Cuentareceptor { get; set; }
+
+        public string Comentariotransaccion { get; set; }
+
+        public Trantipo TipoTransaccion { get; set; }
+
+        public DateTime Fechatransaccion { get; set; }
+
+
+    }
+
+    public enum TransStatus
+    {
+        Fallido,
+        Exitoso,
+        Error
+    }
+    public enum Trantipo
+    {
+        Tranferencia
     }
 }
